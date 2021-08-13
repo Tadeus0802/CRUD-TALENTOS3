a59f0d617f377f6a9904481c1c36da90bc3c61ae
Pano0013 <capoparodi@hotmail.com>
2021-08-13 17:55:52 -0300

    arreglos en el nav y asignar preguntas

--- js/preguntascuestionarios.js
+++ js/preguntascuestionarios.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
             {"data": "idpreguntas"},
             {"data": "descripcion"},
             {"data": "categoria"},
-            { "defaultContent": "<div class='text-center'><div class='btn-group'><button class=''><button class='btn btn-info btn-sm btnAsignar'>Asignar al cuestionario</button></div></div>" }
+            { "defaultContent": "<div class='text-center'><div class='btn-group'><button class='btn btn-info btn-sm btnAsignar'>Asignar al cuestionario</button></div></div>" }
         ],
         'language':{
             "url": "http://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
