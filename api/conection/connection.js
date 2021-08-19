9027fd3faa2ed73ef9138b66f71b60266e618a98
ariel3259 <susanaarcia48@gmail.com>
2021-08-19 14:24:41 -0300

    se utiliza la base de datos del host

--- api/conection/connection.js
+++ api/conection/connection.js
@@ -1,16 +1,10 @@
 const mysql=require('mysql');
 const pool=mysql.createPool({
 	connectionLimit:1000,
-	/*
 	host:"sql248.main-hosting.eu",
 	user:'u836417525_enc',
 	password:'T@lentos2021',
-	database:'u836417525_enc'
-	*/
-	host:"localhost",
-	user:"root",
-	password:"",
-	database:"ct"
+	database:'u836417525_enc'	
 });
 var con=pool;
 module.exports=con;
